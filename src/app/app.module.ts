312d30366535052fdb2ed095efc1e5c695493bb7
jai.coax <in.kousigan@financialexpress.net>
2017-06-20 12:39:12 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,30 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { Page3Component } from './components/page3/page3.component';
+import {routing} from './app.routing';
+import { Page4Component } from './components/page4/page4.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PageNotFoundComponent,
+    Page3Component,
+    Page4Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
